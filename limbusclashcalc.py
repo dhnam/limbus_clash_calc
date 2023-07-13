87e606e7ec00e20bca965d58911d34fb688645bc
LegenDUST <dhnam0502@naver.com>
2023-07-13 19:40:45 +0900

    Translation added

--- limbusclashcalc.py
+++ limbusclashcalc.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from functools import lru_cache
 from math import comb
+from typing import Literal
 import numpy as np
 import PySimpleGUI as sg
 
@@ -88,27 +89,86 @@ def total_avg_power(skill: Skill, prob: list[float]) -> float:
         res += p * sum(coin_power)
     return res
 
-def skill_column(number:int):
- return [[sg.Text(f"캐릭터 {number}", size=10)],
-         [sg.Text("스킬 위력", size=10), sg.Input(key=f"base{number}", size=7)],
-         [sg.Text("코인 갯수", size=10), sg.Input(key=f"count{number}", size=7)],
-         [sg.Text("코인 위력", size=10), sg.Input(key=f"coin{number}", size=7)],
-         [sg.Text("정신력", size=10), sg.Input(key=f"sanity{number}", size=7)],
-         [sg.Text("승률:", size=10), sg.Text("", key=f"winrate{number}", size=7)],
-         [sg.Text("위력 기댓값:", size=10), sg.Text("", key=f"avgpower{number}", size=7)]]
 
-def detail_column(number: int, res: list[float]):
-    res_col = [[sg.Text(f"캐릭터 {number}")]]
-    res_col.extend([[sg.Text(f"남은 코인 {i}개로 승리: {next_prob * 100}%")] for i, next_prob in enumerate(res) if i != 0])
-    res_col.append([sg.Text(f"총 승률: {sum(res) * 100}%")])
+LanguageType = Literal['kr'] | Literal['en']
+SentencePair = dict[LanguageType, str]
+Translation = dict[str, SentencePair]
+
+skill_trans: Translation = {
+    'character': {'kr': '캐릭터 %d', 'en': 'Character %d'},
+    'base': {'kr': '스킬 위력', 'en': 'Skill power'},
+    'count': {'kr': '코인 갯수', 'en': 'Coin count'},
+    'coin': {'kr': '코인 위력', 'en': 'Coin power'},
+    'sanity': {'kr': '정신력', 'en': 'Sanity'},
+    'winrate': {'kr': '승률:', 'en': 'Win rate:'},
+    'avgpower': {'kr': '위력 기댓값:', 'en': 'Expected Power:'},
+}
+
+detail_trans: Translation = {
+    'character': {'kr': '캐릭터 %d', 'en': 'Character %d'},
+    'coins': {'kr': '남은 코인 %d개로 승리: %f%%', 'en': 'Win with %d coin(s) left: %f%%'},
+    'winrate': {'kr': '총 승률: %f%%', 'en': 'Net. Winrate: %f%%'},
+}
+
+main_title_trans: SentencePair = {
+    'kr': '림버스 합 승률 계산기',
+    'en': 'Limbus Clash Winrate Calculator',
+}
+
+detail_title_trans: SentencePair = {
+    'kr': '상세정보',
+    'en': 'Detailed Information',
+}
+
+main_ui_trans: Translation = {
+    'calc': {'kr': '계산', 'en': 'Calculate'},
+    'detail': {'kr': '상세 정보', 'en': 'Detail'},
+    'language': {'kr': 'English', 'en': '한국어'},
+    'change': {'kr': 'Change language', 'en': '언어 변경'}
+}
+
+detail_ui_trans: Translation = {
+    'exit': {'kr': '확인', 'en': 'OK'}
+}
+
+error_trans: SentencePair = {
+    'kr': '숫자를 입력해주세요',
+    'en': 'Please input the number',
+}
+
+def skill_column(number:int, language:LanguageType):
+    return [[sg.Text(skill_trans['character'][language] % (number), size=15)],
+            [sg.Text(skill_trans['base'][language], size=15), sg.Input(key=f"base{number}", size=7)],
+            [sg.Text(skill_trans['count'][language], size=15), sg.Input(key=f"count{number}", size=7)],
+            [sg.Text(skill_trans['coin'][language], size=15), sg.Input(key=f"coin{number}", size=7)],
+            [sg.Text(skill_trans['sanity'][language], size=15), sg.Input(key=f"sanity{number}", size=7)],
+            [sg.Text(skill_trans['winrate'][language], size=15), sg.Text("", key=f"winrate{number}", size=7)],
+            [sg.Text(skill_trans['avgpower'][language], size=15), sg.Text("", key=f"avgpower{number}", size=7)]]
+
+def detail_column(number: int, res: list[float], language:LanguageType):
+    res_col = [[sg.Text(detail_trans['character'][language] % (number))]]
+    res_col.extend([[sg.Text(detail_trans['coins'][language] % (i, next_prob * 100))] for i, next_prob in enumerate(res) if i != 0])
+    res_col.append([sg.Text(detail_trans['winrate'][language] % (sum(res) * 100))])
     return res_col
 
+def main_layout(language:LanguageType):
+    return [[sg.Menu([[main_ui_trans['language'][language], [main_ui_trans['change'][language] + "::language"]]])],
+            [sg.Column(skill_column(1, language)), sg.Column(skill_column(2, language))],
+            [sg.Button(main_ui_trans['calc'][language], key="calc"), sg.Button(main_ui_trans['detail'][language], key="detail")]]
+
+def detail_layout(language:LanguageType):
+    return [[sg.Column([[sg.Column(detail_column(1, a_win, language)), sg.Column(detail_column(2, b_win, language))]], size=(None, 200), scrollable=True, vertical_scroll_only=True)],
+            [sg.Exit(detail_ui_trans['exit'][language], key="Exit")]]
+
 sg.change_look_and_feel('SystemDefaultForReal')
-layout = [[sg.Column(skill_column(1)), sg.Column(skill_column(2))],
-          [sg.Button("계산", key="calc"), sg.Button("상세 정보", key="detail")]]
-window = sg.Window("림버스 합 승률 계산기", layout, finalize=True)
+
+language: LanguageType = 'kr'
+
+layout = main_layout(language)
+window = sg.Window(main_title_trans[language], layout, finalize=True)
                    
 window2 = None
+
 while True:
     win, event, values = sg.read_all_windows()
     if event == sg.WIN_CLOSED or event == 'Exit':
@@ -122,7 +182,7 @@ while True:
             skill_a = Skill(int(values['base1']), int(values['coin1']), int(values['count1']), int(values['sanity1']))
             skill_b = Skill(int(values['base2']), int(values['coin2']), int(values['count2']), int(values['sanity2']))
         except ValueError as e:
-            sg.popup("숫자를 입력해주세요.")
+            sg.popup(error_trans[language])
             continue
         a_win, b_win = win_probability(skill_a, skill_b)
         win["winrate1"].update(f"{sum(a_win) * 100:.3f}%")
@@ -132,10 +192,21 @@ while True:
         if event == "detail":
             if window2 is not None:
                 continue
-            window2 = sg.Window("상세정보", 
-                                   [
-                                       [sg.Column([[sg.Column(detail_column(1, a_win)), sg.Column(detail_column(2, b_win))]], size=(None, 200), scrollable=True, vertical_scroll_only=True)],
-                                       [sg.Exit('확인', key="Exit")]
-                                   ], finalize=True)
+            window2 = sg.Window(detail_title_trans[language], detail_layout(language), finalize=True)
+    if event.split("::")[-1] == "language":
+        if language == 'kr':
+            language = 'en'
+        else:
+            language = 'kr'
+        win_before = window
+        loc = win_before.current_location()
+        window = sg.Window(main_title_trans[language], main_layout(language), location=loc, finalize=True)
+        del values[0]
+        window.fill(values)
+        window["winrate1"].update(win_before["winrate1"].get())
+        window["winrate2"].update(win_before["winrate2"].get())
+        window["avgpower1"].update(win_before["avgpower1"].get())
+        window["avgpower2"].update(win_before["avgpower2"].get())
+        win_before.close()
 
 window.close()
